4ffde4510bf4797a84546ace1d7eba08d0f94838
murrayinglis <murrayinglis101@gmail.com>
2023-04-12 09:19:46 +0200

    Working on GUI

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -38,6 +39,10 @@ public class MainFrame extends JFrame{
 
     public MainFrame()
     {
+	
+	mainPanel = new JPanel();    
+	mainPanel.setBounds(40,80,200,200);    
+        mainPanel.setBackground(Color.gray);    
         setContentPane(mainPanel);
         setTitle("TikTok");
         setSize(800,800);
